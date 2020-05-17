7db606126bf18fd8d39cff2d35bb6c3cdf5c3b45
kenmugy <42069371+kenmugy@users.noreply.github.com>
2020-05-17 09:56:19 +0300

    added backend socket connection

--- server.js
+++ server.js
@@ -6,4 +6,10 @@ const server = require('http').createServer(app);
 const port = process.env.PORT || 3003;
 app.use(express.static(path.join(__dirname, 'public')));
 
+// add socket functionality
+const io = require('socket.io')(server);
+io.on('connection', (socket) => {
+  console.log('client connected');
+});
+
 server.listen(port, () => console.log(`app listenin on port ${port}`));
