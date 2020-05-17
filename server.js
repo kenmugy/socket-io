2f24d5c9a2bd65f4fbc7f3fa87aef062fee2c1b1
kenmugy <42069371+kenmugy@users.noreply.github.com>
2020-05-17 09:49:48 +0300

    added and served public folder with index file

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const server = require('http').createServer(app);
 
 const port = process.env.PORT || 3003;
+app.use(express.static(path.join(__dirname, 'public')));
 
 server.listen(port, () => console.log(`app listenin on port ${port}`));
